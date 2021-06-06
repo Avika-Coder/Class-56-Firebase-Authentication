e216e797b862661790491c0e3d25ef7b4ba260ac
Avika-Coder <76557800+Avika-Coder@users.noreply.github.com>
2021-06-06 20:34:28 +0530

    Add files via upload

---
+++ components/FirebaseConfig.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDd-eqVYqrJKzrMokJ488y0l1yIFPZBQGE",
+    authDomain: "test-bf661.firebaseapp.com",
+    projectId: "test-bf661",
+    storageBucket: "test-bf661.appspot.com",
+    messagingSenderId: "528440416036",
+    appId: "1:528440416036:web:18a2b64fe269c3140285b1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  export default firebase;
